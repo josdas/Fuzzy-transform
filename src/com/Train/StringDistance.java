7da23a747ce1e829e05014c19b3402e77b57841d
josdas <josdas@mail.ru>
2017-07-01 16:22:56 +0300

    bug has been fixed

--- src/com/Train/StringDistance.java
+++ src/com/Train/StringDistance.java
@@ -11,14 +11,15 @@ public class StringDistance {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
+                dp[i][j] = Double.POSITIVE_INFINITY;
                 if (a.charAt(i - 1) == b.charAt(j - 1)) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
                 }
-                dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
-                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+                dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
             }
         }
-        return 2 * dp[n][m] / (n + m);
+        return dp[n][m];
     }
 
     public static double levenshteinSwap(String a, String b) {
@@ -28,16 +29,17 @@ public class StringDistance {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
+                dp[i][j] = Double.POSITIVE_INFINITY;
                 if (a.charAt(i - 1) == b.charAt(j - 1)) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
                 }
                 if (i > 1 && j > 1
                         && a.charAt(i - 2) == b.charAt(j - 1)
                         && a.charAt(i - 1) == b.charAt(j - 2)) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 2][j - 2] + 0.9);
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 2][j - 2] + 0.5);
                 }
-                dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
-                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+                dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
             }
         }
         return dp[n][m];
