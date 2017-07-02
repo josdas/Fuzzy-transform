114aa8a1851559accc6d6c07a526786207b5e4de
josdas <josdas@mail.ru>
2017-07-02 14:09:03 +0300

    removed const in code

--- src/com/Train/AbsTraining.java
+++ src/com/Train/AbsTraining.java
@@ -9,8 +9,6 @@ import java.util.function.UnaryOperator;
  * Created by josdas on 30.06.2017.
  */
 public abstract class AbsTraining<T extends WordConvector> {
-    private static final double START_DH = 5;
-    private static final double START_N = 7;
 
     public static final UnaryOperator<Double> ACTIVE_F_A = (Double a) -> {
         return 1.0 / (1 + Math.exp(-a));
@@ -19,12 +17,9 @@ public abstract class AbsTraining<T extends WordConvector> {
         return (Math.exp(a) - Math.exp(-a)) / (Math.exp(a) + Math.exp(-a));
     };
 
-    protected T convector;
-    protected Random random;
-
-    protected double dh = START_DH;
-    protected double n = START_N;
-    protected double result;
+    T convector;
+    Random random;
+    double result;
 
     public AbsTraining(T convector) {
         this.convector = convector;
--- src/com/Train/TrainingWNN.java
+++ src/com/Train/TrainingWNN.java
@@ -10,6 +10,16 @@ import com.NeuralNetwork.WordNN;
  * Created by josdas on 30.06.2017.
  */
 public class TrainingWNN extends AbsTraining<WNNConvector> {
+    private static final double START_DH = 5;
+    private static final double START_N = 7;
+    private static final double DH_DECREASE_K = 0.9999;
+    private static final double N_DECREASE_K = 0.9999;
+    private static final double DH_MIN = 0.1;
+    private static final double N_MIN = 1;
+
+    private double dh = START_DH;
+    private double n = START_N;
+
     private Option option;
     private EvalWNN evaluation;
 
@@ -32,25 +42,29 @@ public class TrainingWNN extends AbsTraining<WNNConvector> {
         // simple random gradient descent
 
         for (int iteration = 0; iteration < numberIterations; iteration++) {
-            Coefficient[] coefficient = convector.getCoefficient(); // work with clone of the coefficients
+            // work with clone of the coefficients
+            Coefficient[] coefficient = convector.getCoefficient();
 
-            for (Coefficient aCoefficient : coefficient) { // do small change of the arrays
-                int m = random.nextInt((int) n + 1) + 1;
+            // do small change of the arrays
+            for (Coefficient aCoefficient : coefficient) {
+                int m = random.nextInt((int) (n + N_MIN));
                 for (int j = 0; j < m; j++) {
                     int t = random.nextInt(aCoefficient.summarySize());
                     double x = aCoefficient.get(t);
-                    x += (random.nextDouble() - 0.5) * (dh + 0.1);
+                    x += (random.nextDouble() - 0.5) * (dh + DH_MIN);
                     aCoefficient.set(t, x);
                 }
             }
             WordNN newNN = new WordNN(coefficient[0], coefficient[1], option);
             WNNConvector nConvector = new WNNConvector(newNN);
-            double nRes = evaluation.eval(nConvector); // calc function of the NN
+
+            // calc function of the NN
+            double nRes = evaluation.eval(nConvector);
             if (nRes > result) {
                 result = nRes;
                 convector = nConvector;
-                dh *= 0.9999;
-                n *= 0.9999;
+                dh *= DH_DECREASE_K;
+                n *= N_DECREASE_K;
             }
         }
         //evaluation.generation();
