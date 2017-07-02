af41a668976051b3835987397086632102439b68
josdas <josdas@mail.ru>
2017-07-02 17:09:57 +0300

    refactoring

--- src/com/Testing.java
+++ src/com/Testing.java
@@ -6,6 +6,8 @@ import com.NeuralNetwork.Option;
 import com.Train.AbsTraining;
 import com.Train.TrainingWNN;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 /**
@@ -14,18 +16,39 @@ import java.util.Scanner;
 
 
 //done применить сверточную идею для хеширования любых строк
-//todo играть с метриками
+//done играть с метриками
 //done сделать обучение с levenshtein
 //todo геометрический поиск ближайших точек через случайные прямые
 //todo словарь + быстрый приближенный поиск (можно делать точный поиск по ближайшим)
 //done файлы
 
 public class Testing {
-    public static final int ALP = 3;
-    public static final int NEURONS_FOR_LETTER = 10;
-    private static final int MAX_TIME = 60 * 10;
+    private static final int ALP = 3;
+    private static final int NEURONS_FOR_LETTER = 10;
+    private static final long MAX_TIME = 60;
 
-    public static void main(String[] args) {
+    private static TrainingWNN readTWNNFromFile() {
+        Coefficient coefficientF = null;
+        Coefficient coefficientS = null;
+        Option option = new Option(
+                AbsTraining.ACTIVE_F_A,
+                ALP,
+                NEURONS_FOR_LETTER
+        );
+        try {
+            coefficientF = new Coefficient("First.txt");
+            coefficientS = new Coefficient("Second.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("Fail to read the NN");
+            System.exit(1);
+        }
+        return new TrainingWNN(
+                coefficientF,
+                coefficientS,
+                option
+        );
+    }
+    private static TrainingWNN generationRandomTWNN() {
         Coefficient coefficient = new Coefficient(
                 NEURONS_FOR_LETTER + 1,
                 15,
@@ -36,32 +59,46 @@ public class Testing {
                 ALP,
                 NEURONS_FOR_LETTER
         );
-        TrainingWNN trainingWNN = new TrainingWNN(
+        return new TrainingWNN(
                 coefficient,
                 coefficient,
                 option
         );
+    }
 
+    private static void train(TrainingWNN trainingWNN, long maxTime) {
+        System.out.println("Start training for " + maxTime + " seconds");
         long startTime = System.currentTimeMillis();
         while (true) {
             long timeSpent = (System.currentTimeMillis() - startTime) / 1000;
-            if (timeSpent > MAX_TIME) {
+            System.out.println("Time spent: " + timeSpent + " seconds");
+            if (timeSpent > maxTime) {
                 break;
             }
-            System.out.println("Time spent: " + timeSpent + " second");
             trainingWNN.train(100);
         }
-
-        WNNConvector result = trainingWNN.getConvector();
-        Scanner scan = new Scanner(System.in);
-
         System.out.println("Training has been finished");
+    }
+    private static void saveWNNToFile(WNNConvector result) {
+        try {
+            result.getCoefficient()[0].save("First.txt");
+            result.getCoefficient()[1].save("Second.txt");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            System.out.println("Fail to saveWNNToFile the NN");
+        }
+    }
+    public static void main(String[] args) {
+        TrainingWNN trainingWNN = readTWNNFromFile();
+        train(trainingWNN, MAX_TIME);
+        WNNConvector result = trainingWNN.getConvector();
+        saveWNNToFile(result);
+
+        Scanner scan = new Scanner(System.in);
         while (true) {
             String strA = scan.nextLine();
             String strB = scan.nextLine();
             double[] pointA = result.get(strA);
             double[] pointB = result.get(strB);
-
             System.out.println(VectorDistance.distance(pointA, pointB) + " "
                     + StringDistance.levenshtein(strA, strB));
         }
